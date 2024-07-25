7770bb60a28a61fa651b8c760201f282d9bee8e3
CSRookie <ztcsrookie@gmail.com>
2024-07-26 00:37:00 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mablars',  # 库的名称
-    version='0.1.0',  # 库的版本
-    packages=find_packages(),  # 自动发现所有的包
+    name='mablars',
+    version='0.1.0',
+    packages=find_packages(),
     install_requires=[
-        # 在这里列出你的库依赖的第三方包，例如:
-        # 'requests',
+        'numpy',
+        'matplotlib',
+        'causal-learn',
+        'scikit-learn',
+        'python-weka-wrapper3'
     ],
-    description='This is a python package of mablars',  # 库的简短描述
+
+    description='This is a python package of mablars',
     long_description=open('README.md').read(),  # 从 README 文件读取详细描述
     long_description_content_type='text/markdown',  # README 文件的格式
-    author='Te Zhang',  # 你的名字
-    author_email='ztcsrookie@gmail.com',  # 你的邮箱
-    url='https://github.com/yourusername/my_library',  # 你的库的主页
+    author='Te Zhang',
+    author_email='ztcsrookie@gmail.com',
+    url='https://github.com/ztcsrookie/mablars',
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System :: MacOS :: MacOS X',
     ],
 )
